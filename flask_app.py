c3069a75d0113b0c75e0852460665d1c2abe075d
stevee-bee <77158150+stevee-bee@users.noreply.github.com>
2021-09-26 06:16:43 +0000

    Write current user to the comments table. Read/display username for each listed comment.

--- flask_app.py
+++ flask_app.py
@@ -5,9 +5,9 @@
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, LoginManager, logout_user, UserMixin
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -28,6 +28,7 @@ app.secret_key = "Qww#c0@d6&e78c*qAYeo"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 class User(UserMixin, db.Model):
 
     __tablename__ = "users"
@@ -35,7 +36,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128))
     password_hash = db.Column(db.String(128))
-
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
@@ -57,6 +57,8 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
     posted = db.Column(db.DateTime, default=datetime.now)
+    commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    commenter = db.relationship('User', foreign_keys=commenter_id)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -67,7 +69,7 @@ def index():
     if not current_user.is_authenticated:
         return redirect(url_for('index'))
 
-    comment = Comment(content=request.form["contents"])
+    comment = Comment(content=request.form["contents"], commenter=current_user)
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('index'))
